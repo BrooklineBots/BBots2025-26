fa6cb15f30fe898f18b4f47838a16fc04244692d
briellegithub <briellehwang@gmail.com>
2026-08-13 17:35:05 -0400

    Added Brogan's Code (WIP)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/AutoAlignCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/AutoAlignCommand.java
@@ -24,7 +24,50 @@ public class AutoAlignCommand extends CommandBase {
         double kP = 0.002;
         double error = 0;
         double lastError = 0;
-        double goalX = 0;
+        double goalX = 0; //offset here
+        double angleTolerance = 0.4;
+        double currTime = 0;
+        double lastTime = 0;
+
+        public void start(){
+            resetRuntime();
+            currTime = getRuntime();
+        }
+
+        //LOOP
+
+        AprilTagDetection id20 = webcam.getTagBySpecificId(20);
+
+        if(gamepad1.left_trigger > 0.3){
+            if(id20 != null){
+                error = goalX - id20.ftcPose.bearing; //tx
+
+                if(MTh.abs(err0r)< angleTo tolerance){
+                    rotate = 0;
+                } else {
+                    double pTerm = error * kP;
+
+                    cufrTime = getRuntime();
+
+                    double dT = currTime - lastTIme;
+                    double dTerm = ((error - lastError)/dT) * kD;
+                    rotate = Range.clip(pTerm + dTerm, -0.4, 0.4);
+                    lastError = error;
+                    lastTIme = currTime;
+                }
+
+            }
+            else{
+                lastTIme =getRuntime();
+                lastError = 0;
+
+            }
+        }
+        else{
+            lastTIme =getRuntime();
+            lastError = 0;
+
+        }
         
         telemetry.update();
     }
@@ -33,4 +76,6 @@ public class AutoAlignCommand extends CommandBase {
     public void end(boolean interrupted) {
         drive.stopMotors();
     }
+
+
 }
